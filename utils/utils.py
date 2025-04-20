aea2891978d497e01a8c8af8b40ecc17851e495f
xercesm <belaya.aliss@gmail.com>
2025-04-20 19:00:54 +0300

    добавлено кеширование запроса к бд

--- utils/utils.py
+++ utils/utils.py
@@ -1,7 +1,11 @@
 # utils/utils.py
 
 from db.db_utils import get_admin_by_tg_id
+from cachetools import TTLCache, cached
 
+cache = TTLCache(maxsize=100, ttl=30)
+
+@cached(cache)
 def is_admin(tg_user_id):
     admin = get_admin_by_tg_id(tg_user_id)
     return admin is not None
