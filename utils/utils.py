aae8569ea001c59b7d13b02238bb812027d4eb42
xercesm <belaya.aliss@gmail.com>
2025-04-22 18:25:26 +0300

    добавлен метод для удаления списка сообщений

--- utils/utils.py
+++ utils/utils.py
@@ -1,11 +1,23 @@
 # utils/utils.py
+from aiogram import Bot
+from aiogram.fsm.context import FSMContext
 
 from db.db_utils import get_admin_by_tg_id
-from cachetools import TTLCache, cached
-
-cache = TTLCache(maxsize=100, ttl=30)
-
-@cached(cache)
 def is_admin(tg_user_id):
     admin = get_admin_by_tg_id(tg_user_id)
     return admin is not None
+
+
+async def delete_saved_messages(bot: Bot, chat_id: int, state: FSMContext):
+    data = await state.get_data()
+    messages = data.get("messages_for_deletion", [])
+    print(messages)
+
+    for msg_id in messages:
+        try:
+            await bot.delete_message(chat_id=chat_id, message_id=msg_id)
+        except Exception:
+            pass
+
+    # Очистим список
+    await state.update_data(messages_to_delete=[])
